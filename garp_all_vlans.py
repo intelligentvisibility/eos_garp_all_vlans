30fe0a2f10b7e88cf7b768079d0af386d79927e4
Richard Collins <rbcollins@gmail.com>
2017-06-09 17:10:13 -0400

    fix PEP8 issues

--- garp_all_vlans.py
+++ garp_all_vlans.py
@@ -19,10 +19,11 @@ notify all endpoints to use the new Arista default gateway for example.
 
 """
 
-# set the frequency of pings in seconds to send (CTRL-C allows for early termination when desired)
-# (ex: can use .5 for 500ms between garps)
+# set the frequency of pings in seconds to send (CTRL-C allows for early
+# termination when desired) (ex: can use .5 for 500ms between garps)
 ARPING_INTERVAL = 1
-# flag to control output of ARPING.  True = stdout/stderr, False = send to /dev/null
+# flag to control output of ARPING.
+# True = stdout/stderr, False = send to /dev/null
 ARPING_OUTPUT = False
 
 # set logging level to desired output
@@ -43,7 +44,8 @@ def kill_popen_list(popen_list):
 
 def signal_handler(signal, frame):
     """
-    register a signal handler to catch SIGINT and kill all the child arping processes
+    register a signal handler to catch SIGINT and kill all the child arping
+    processes
     """
     print('CTRL-C was pressed!\n\nKilling child processes...')
     global RUN_FLAG
@@ -67,9 +69,11 @@ while RUN_FLAG:
     logging.debug("Starting new ARPING for all VLAN interfaces")
     # pull a list of all interfaces on the switch
     interface_list = netifaces.interfaces()
-    # build a list of tuples as (interface, ipaddress) to be used for calling arping on
+    # build a list of tuples as (interface, ipaddress) to be used for calling
+    #  arping on
     # all vlan interfaces
-    target_list = [(interface, netifaces.ifaddresses(interface)[2][0]['addr']) for interface in interface_list if
+    target_list = [(interface, netifaces.ifaddresses(interface)[2][0]['addr'])
+                   for interface in interface_list if
                    str(interface)[:4].lower() == 'vlan']
     # kick off a ping on each interface and store the list of processes
     process_count = 0
@@ -77,22 +81,26 @@ while RUN_FLAG:
         with open(os.devnull, 'w') as dev_null:
             for target_network in target_list:
                 process_list.append(subprocess.Popen(
-                    ['/sbin/arping', '-A', '-c', '1', '-I', str(target_network[0]), str(target_network[1])],
+                    ['/sbin/arping', '-A', '-c', '1', '-I',
+                     str(target_network[0]), str(target_network[1])],
                     stdout=dev_null, stderr=subprocess.STDOUT))
                 process_count += 1
     else:
         for target_network in target_list:
             process_list.append(subprocess.Popen(
-                ['/sbin/arping', '-A', '-c', '1', '-I', str(target_network[0]), str(target_network[1])]))
+                ['/sbin/arping', '-A', '-c', '1', '-I', str(target_network[0]),
+                 str(target_network[1])]))
             process_count += 1
-    logging.debug("Started {} arping processes for {} interfaces.".format(process_count, len(target_list)))
+    logging.debug("Started {} arping processes for "
+                  "{} interfaces.".format(process_count, len(target_list)))
 
     # ensure that all the processes have exited before continuing
     while len(process_list):
         for process in process_list:
             if process.poll() is not None:
                 process_list.remove(process)
-    logging.info("Execution time for all {} processes: {} seconds".format(process_count, time.time() - start_time))
+    logging.info("Execution time for all {} processes: "
+                 "{} seconds".format(process_count, time.time() - start_time))
     # sleep for requested interval before sending the next ping
     logging.info("Sleeping for {} seconds".format(ARPING_INTERVAL))
     time.sleep(ARPING_INTERVAL)
